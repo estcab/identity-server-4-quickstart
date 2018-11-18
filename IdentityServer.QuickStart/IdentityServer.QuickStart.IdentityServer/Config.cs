9d6878031b853cd78ffd30c8872be9af4b93b623
Esteban Cabal <estcab@gmail.com>
2018-11-18 08:49:18 +0100

    Add Configuration for IdentityServer

---
+++ IdentityServer.QuickStart/IdentityServer.QuickStart.IdentityServer/Config.cs
@@ -0,0 +1,31 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer.QuickStart.IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            yield return new ApiResource("api_1", "My API");
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            yield return new Client
+            {
+                ClientId = "client_1",
+
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                AllowedScopes = { "api_1" }
+
+            };
+        }
+    }
+}
--- IdentityServer.QuickStart/IdentityServer.QuickStart.IdentityServer/Startup.cs
+++ IdentityServer.QuickStart/IdentityServer.QuickStart.IdentityServer/Startup.cs
@@ -15,7 +15,10 @@ namespace IdentityServer.QuickStart.IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentityServer().AddDeveloperSigningCredential();
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
 
         }
 
